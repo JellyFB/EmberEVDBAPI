f5cd58e17d2b5f738f4b08cf43fcf6b4ed88b618
JellyFB <131621190+JellyFB@users.noreply.github.com>
2025-03-02 02:41:49 -0800

    updating IP port to make requests

--- SQLTestScript.js
+++ SQLTestScript.js
@@ -36,6 +36,6 @@ app.get('/api/get-data', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
